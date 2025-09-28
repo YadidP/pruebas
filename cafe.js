b5b57f52a808a12d68e1729667762987f31f776b
Yadira Peredo <yadira.peredo.30.3s4@gmail.com>
2025-09-28 19:00:01 -0400

    Añadiendo nuevo script

---
+++ cafe.js
@@ -0,0 +1,10 @@
+// 1. Declaración de variables
+let numero1 = 15;
+let numero2 = 10;
+
+// 2. Proceso o cálculo
+let resultadoSuma = numero1 + numero2;
+
+// 3. Salida de datos (mostrar el resultado)
+console.log("¡Hola! La suma de los dos números es:");
+console.log(resultadoSuma);
